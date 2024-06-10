4f0d3692dbc1f397b17e24879164df34828497bf
nicolòdegiorgi <165511651+NicoloDeGiorgi@users.noreply.github.com>
2024-06-10 20:09:53 +0200

    add :string e ciclo for

--- src/main/java/com/ferragnez/party/CheckGuest.java
+++ src/main/java/com/ferragnez/party/CheckGuest.java
@@ -1,10 +1,38 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		String [] invitatiFerragnez = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		
+		Scanner scanneer = new Scanner(System.in);
+		
+		   System.out.print("Mi dica il suo nome gentilmente:");
+		   
+		   String name = scan.nextLine();
+		   
+		   
+		   boolean invitato = false;
+		   
+		   for (int i = 0; i < invitatiFerragnez.length; i++) {
+			   if (name.equals(invitatiFerragnez[i])) {
+				   invitato = true;
+			   }
+		   }
+		   
+		   if (invitato) {
+			   System.out.println("Benvenuto/a, si prenda un drink e si goda la festa");
+		   }
+		   else 
+			   System.out.println("Lei non è invitato, torni a casa o chiamo la sicurezza");
+		   
+		   scan.close();
+		 
 	}
+	
 
 }
